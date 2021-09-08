7a241f7dcd5a7a537be05c7b7106902551d4eb1b
AJPascoe <pascoe312@gmail.com>
2021-09-08 11:59:28 +0100

    connencting to mongo atlas

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,11 +4,19 @@ require("dotenv").config();
 const client = new MongoClient(process.env.MONGO_URI)
 
 const connection = async (crudFunc, dataObj) => {
+    try{
     await client.connect();
-    const db = client.db("movies")
-    const collection = db.collection("movie")
-    await crudFunc(collection, dataObj)
-    client.close()
+    const db = client.db("movies");
+
+    const collection = db.collection("movie");
+    await collection.insertOne({name: "Adam"});
+
+    // await crudFunc(collection, dataObj)
+    client.close();
+} catch (error){
+    console.log(error);
 }
+};
+connection();
 
 module.export = connection;
