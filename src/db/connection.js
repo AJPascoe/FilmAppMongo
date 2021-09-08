bee37e578ab90c37dfb7f1154b710738c0fb0512
AJPascoe <pascoe312@gmail.com>
2021-09-08 11:46:06 +0100

    connected to mongoDB

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,3 +3,12 @@ require("dotenv").config();
 
 const client = new MongoClient(process.env.MONGO_URI)
 
+const connection = async (crudFunc, dataObj) => {
+    await client.connect();
+    const db = client.db("movies")
+    const collection = db.collection("movie")
+    await crudFunc(collection, dataObj)
+    client.close()
+}
+
+module.export = connection;
