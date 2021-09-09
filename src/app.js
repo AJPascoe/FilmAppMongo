177e44e56b1c5e6767aab57f2a84116ee0ea635d
AJPascoe <pascoe312@gmail.com>
2021-09-09 10:25:28 +0100

    adding update and delete function

--- src/app.js
+++ src/app.js
@@ -11,9 +11,45 @@ const app = async () => {
       year: process.argv[6],
     };
     await connection(addMovie, newMovie);
-  } else if (command === "list"){
-      await connection(listMovies);
+  } else if (command === "list") {
+    await connection(listMovies);
+  }  else if (command === "update") {
+    const updateObj = {
+      filter: process.argv[3],
+      like: process.argv[4],
+    };
+    await connection(updateMovie, updateObj);
+  } else if (command === "delete") {
+    await connection(deleteMovie, { filter: process.argv[3] });
+  } else {
+    console.log("Incorrect command");
   }
+
+
+
+
+
+
+
+
+
+  // } else if (command === "del") {
+  //   const delMovie = {
+  //     name: process.argv[3]
+  //     // actor: process.argv[4],
+  //     // like: process.argv[5],
+  //     // year: process.argv[6],
+  //   };
+  //   await connection(delMovie, input[3]);
+  // } else if (command === "update") {
+  //   const updateMovie = {
+  //     name: process.argv[3],
+  //     actor: process.argv[4],
+  //     like: process.argv[5],
+  //     year: process.argv[6],
+  //   };
+  //   await connection(updateMovie, newMovie);
+  // }
 };
 
 app();
--- src/utils/index.js
+++ src/utils/index.js
@@ -10,3 +10,41 @@ exports.listMovies = async (collection) => {
     const list = await collection.find({}).toArray();
     console.log(list);
 };
+
+
+
+exports.updateMovie = async (collection, updateObj) => {
+    await collection.updateOne(
+      { name: updateObj.filter },
+      { $set: { like: updateObj.like } }
+    );
+  };
+  
+  exports.deleteMovie = async (collection, deleteObj) => {
+    await collection.deleteOne({ name: deleteObj.filter });
+  };
+
+// exports.updateMovie = async (collection, dataObj)=>{
+//     try{
+//         await collection.insertOne(dataObj)
+//     }catch (error){
+//         console.log(error);
+//     }
+
+// }
+
+// exports.updateMovie = (list, input) => {
+//     const list = await collection.find({}).toArray();
+//     console.log(list);
+//     let updateMovie;
+//     console.log(list)
+//     list.map((movie, index) => {
+//         if (movie.name === input) {
+//             updateMovie = index;
+//         }
+//         let stringMovieList = JSON.stringify(list);
+//         fs.writeFileSync('./db/storage.json', stringMovieList);
+//         console.log(list)
+//     });
+   
+// };
